10a76f18f446262f600a901a7ebee20ea6d9363e
wufs <874520180@qq.com>
2022-08-16 17:15:25 +0800

    wip:更新8.vite环境变量配置.md文档

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import viteBaseConfig from "./vite.base.config";
 import viteProdConfig from "./vite.prod.config";
 import viteDevConfig from "./vite.dev.config";
@@ -16,10 +16,12 @@ const envResolver = {
         return Object.assign({}, viteBaseConfig, viteDevConfig)//将多个配置合并到一起
     }
 }
-export default defineConfig(({command,}) => {
+export default defineConfig(({command,mode}) => {
     // console.log("command", command)
     // console.log("process",process.env)
-    console.log("process",process.cwd())
-    // const env =loadEnv(mode,process.cwd())
+    // console.log("process",process.cwd());
+    // console.log("mode",mode);
+    const env =loadEnv(mode,process.cwd(),"");
+    console.log("env",env);
     return envResolver[command]();
 })
